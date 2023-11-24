71f2da1fc0ee50fa780110334ebe177b0dbe41cc
vitorPr0 <senaivitor899@gmail.com>
2023-11-24 08:42:45 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SistemaVFR.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+        //public DbSet<Paciente> Paciente { get; set; }
+        //public DbSet<Profissional> Profissional { get; set; }
+        //public DbSet<Cargo> Cargo { get; set; }
+        //public DbSet<Estado> Estado { get; set; }
+        //public DbSet<Cidade> Cidade { get; set; }
+        //public DbSet<Resultado> Resultado { get; set; }
+        //public DbSet<Exame> Exame { get; set; }
+    }
+}
